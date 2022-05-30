0c8ed5a5752205faac66a2660971d003c3752782
romainplt <romain.pialat@imt-atlantique.net>
2022-05-30 19:11:17 +0200

    Tried the pairing stuff and it seems to work
    Now i have to understand it lol

---
+++ Pairing.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <gmp.h>
+#include <tgmath.h>
+#include "Cyclic_group.cpp"
+#include "pbc-0.5.14/include/pbc.h"
+using namespace std;
+
+int main() {
+    pairing_t pairing;
+    char param[1024];
+    size_t count = fread(param, 1, 1024, stdin);
+    if (!count) pbc_die("input error");
+    pairing_init_set_buf(pairing, param, count);
+
+    element_t g, h;
+    element_t public_key, secret_key;
+    element_t sig;
+    element_t temp1, temp2;
+
+    element_init_G2(g, pairing);
+    element_init_G2(public_key, pairing);
+    element_init_G1(h, pairing);
+    element_init_G1(sig, pairing);
+    element_init_GT(temp1, pairing);
+    element_init_GT(temp2, pairing);
+    element_init_Zr(secret_key, pairing);
+
+    element_random(g);
+    element_random(secret_key);
+    element_pow_zn(public_key, g, secret_key);
+    const char* data = "ABCDEF";
+    void* datap = &data;
+    element_from_hash(h, datap, 6);
+    element_pow_zn(sig, h, secret_key);
+
+    pairing_apply(temp1, sig, g, pairing);
+    pairing_apply(temp2, h, public_key, pairing);
+    if (!element_cmp(temp1, temp2)) {
+        printf("signature verifies\n");
+    } else {
+        printf("signature does not verify\n");
+    }
+    return 0;
+}
